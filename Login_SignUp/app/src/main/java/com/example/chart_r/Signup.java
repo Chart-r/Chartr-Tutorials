0aefc3d74cbe702aff1b513f4106184fc09b200b
Gabriel <gabriel.hayat@epfl.ch>
2018-02-18 15:30:28 -0600

    Tutorials

---
+++ Login_SignUp/app/src/main/java/com/example/chart_r/Signup.java
@@ -0,0 +1,13 @@
+package com.example.chart_r;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
